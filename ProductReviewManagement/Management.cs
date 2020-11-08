8f0b3f9010504b010d80b9208c1bdc8602984ca1
Suraj <surajshrm000@gmail.com>
2020-11-08 23:02:38 +0530

    [Suraj] Add: Retrieve count for each productID

--- ProductReviewManagement/Management.cs
+++ ProductReviewManagement/Management.cs
@@ -30,5 +30,13 @@ namespace ProductReviewManagement
                 Console.WriteLine(list.ToString());
             }
         }
+        public void RetrieveCountOfRecords(List<ProductReview> listProductReview)
+        {
+            var recordedData = listProductReview.GroupBy(x => x.ProductID).Select(x => new { ProductID = x.Key, Count = x.Count() });
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine(list.ProductID + " " + list.Count);
+            }
+        }
     }
 }
